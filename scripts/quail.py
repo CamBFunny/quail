d1f33bbcf41e77f37d98983f47de08515be68e2a
CameronBFunny <156382194+CamBFunny@users.noreply.github.com>
2025-12-17 09:46:49 -0600

    Update quail.py

--- scripts/quail.py
+++ scripts/quail.py
@@ -57,5 +57,3 @@ with open(file_path, 'a') as file:  # a - add new line
 with open(file_path, 'r') as file:
     content = file.read() # Reads the entire file content into a single string
     print(content)
-
-print()
