4e81ffab94d6bf6c692b4dd80923de7664e2e529
CameronBFunny <156382194+CamBFunny@users.noreply.github.com>
2026-01-16 09:05:32 -0600

    Update bloody yin yang.py

--- art/animations/bloody yin yang.py
+++ art/animations/bloody yin yang.py
@@ -1,5 +1,5 @@
 # Yin-Yang battling over the screen
-import pygame
+import pygame, random
 pygame.init()
 resolution = [800, 600]
 screen = pygame.display.set_mode(resolution)
@@ -9,12 +9,18 @@ framerate = 60
 dt = 0
 
 size = 40
-position = [0, 0]
 rate = 350
-rate_x = rate
-rate_y = rate
 LeftClick = False
 button_check = False
+
+position = [0, random.choice(range(resolution[1]))]
+rate_x = rate
+rate_y = rate
+
+location = [resolution[0]-size, random.choice(range(resolution[1]))]
+speed_x = -rate
+speed_y = -rate
+
 
 grid_size = [resolution[0] // size, resolution[1] // size]
 box = [[0] * grid_size[1]] * grid_size[0]
@@ -40,12 +46,14 @@ while running:  # Game Loop
             boundary = [n*size, m*size]
             if box[n][m] == 0:
                 color = (0, 0, 0)
-            else:
+                if abs(boundary[0] - location[0]) < size and abs(boundary[1] - location[1]) < size and speed_x < 0:
+                    box[n][m] = 1
+                    speed_x = -speed_x 
+            elif box[n][m] == 1:
                 color = (255, 255, 255)
-                if abs(boundary[0] - position[0]) < size and abs(boundary[1] - position[1]) < size:
+                if abs(boundary[0] - position[0]) < size and abs(boundary[1] - position[1]) < size and rate_x > 0:
                     box[n][m] = 0
-                    rate_x = -rate_x
-                    position[0] += dt * rate_x
+                    rate_x = -rate_x 
             pygame.draw.rect(screen, color, (n*size, m*size, size, size))
 
     if position[0] + size >= screen.get_width() and rate_x > 0:
@@ -56,15 +64,29 @@ while running:  # Game Loop
         rate_y = -rate_y
     if position[1] <= 0 and rate_y < 0:
         rate_y = -rate_y
+        
+
+    if location[0] + size >= screen.get_width() and speed_x > 0:
+        speed_x = -speed_x
+    if location[0] <= 0 and speed_x < 0:
+        speed_x = -speed_x
+    if location[1] + size >= screen.get_height() and speed_y > 0:
+        speed_y = -speed_y
+    if location[1] <= 0 and speed_y < 0:
+        speed_y = -speed_y
 
     position[0] += dt * rate_x
     position[1] += dt * rate_y
     pygame.draw.rect(screen, (255, 255, 255), (position[0], position[1], size, size))
+    
+    location[0] += dt * speed_x
+    location[1] += dt * speed_y
+    pygame.draw.rect(screen, (0, 0, 0), (location[0], location[1], size, size))
 
     if event.type == pygame.MOUSEBUTTONUP:
         button_check = False
 
-    mod = 1.002
+    mod = 1.0002
     rate_x *= mod
     rate_y *= mod
 
