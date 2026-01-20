17fbced4c0a696a8c8ab6f44a96ce370b0ef3b1b
CamBFunny <cameron.beisel@gmail.com>
2026-01-19 21:49:40 -0600

    pinball physics working

--- games/pynball.py
+++ games/pynball.py
@@ -10,21 +10,27 @@ clock = pygame.time.Clock()
 framerate = 60
 dt = 0
 
-# cosmic = pygame.image.load('lib/cosmic.jpg')
-# sz = cosmic.get_size()
-# scl = 200/sz[0]
-# cosmic = pygame.transform.scale(cosmic, (sz[0] * scl, sz[1] * scl)) 
+cosmic = pygame.image.load('lib/cosmic.jpg')
+sz = cosmic.get_size()
+scl = 200/sz[0]
+cosmic = pygame.transform.scale(cosmic, (sz[0] * scl, sz[1] * scl))
 
 dimensions = [80, 120]
 scale = resolution[1] / dimensions[1]
 size = [dimensions[0] * scale, dimensions[1] * scale]
-
+border = 10
+edges = [int((resolution[0] - size[0]) / 2 + border * 2), int((resolution[0] + size[0]) / 2 - border * 2)]
 LeftClick = False
 RightClick = False
 escape = False
-button_check = False 
+button_check = False
 
-gravity = 30
+font_mono20 = pygame.font.SysFont("Mono", 20, bold=False, italic=False)
+font_helv20 = pygame.font.SysFont("Helvetica", 20, bold=False, italic=False)
+
+def draw_text(text, font, text_col, x, y):  # function for outputting text onto the screen
+    img = font.render(text, True, text_col)
+    screen.blit(img, (x, y))
 
 class Pinball():
     def __init__(self, pos, color, size, speed):
@@ -37,7 +43,7 @@ class Pinball():
         pygame.draw.circle(screen, self.color, self.pos, self.size)
         
     def fall(self, interval):
-        self.speed = [self.speed[0], self.speed[1] + gravity * interval]
+        self.speed[1] = self.speed[1] + gravity * interval
         self.pos = [self.pos[0] + self.speed[0], self.pos[1] + self.speed[1]]
         
 class Flipper():
@@ -61,15 +67,27 @@ class Bumper():
     def draw(self):
         pygame.draw.circle(screen, self.color, self.pos, self.size)
         
-launcher = [resolution[0]- 225, resolution[1] - 50]
+launcher = [resolution[0]- 230, resolution[1] - 50]
 
-ball = Pinball(launcher, (255, 255, 255), 7, [0, -30])
+ball = Pinball(launcher, (255, 255, 255), 7, [0, -40])
+score = 0
 
-bump1 = Bumper([585, 300], (150, 150, 150), 12)
+gravity = 30
+bumpers = [Bumper([575, 25], (150, 150, 150), 13),]
 
-# Debug
-# ball.speed[1] = -3
-# gravity = 0
+cluster = [420, 240]
+bumpers.append(Bumper([cluster[0] + 40, cluster[1]], (150, 150, 150), 12))
+bumpers.append(Bumper([cluster[0], cluster[1] + 20], (150, 150, 150), 12))
+bumpers.append(Bumper([cluster[0] - 40, cluster[1]], (150, 150, 150), 12))
+
+for p in range(random.choice(range(3, 5))):
+    x = random.choice(range(edges[0]+20, edges[1]-40))
+    y = random.choice(range(150, 400))
+    bumpers.append(Bumper([x, y],(150, 150, 150), 12))
+
+# debug
+# ball.speed[1] = -20
+# gravity = 9
 
 while running:  # Game Loop
     for event in pygame.event.get():
@@ -90,14 +108,12 @@ while running:  # Game Loop
         button_check = True
 
     screen.fill([0, 0, 0])
-    # screen.blit(cosmic, [0, 0])
+    screen.blit(cosmic, [0, 0])
     
-    border = 10
     pygame.draw.rect(screen, [50, 50, 50], [resolution[0]/2 - size[0]/2, 0, size[0], size[1]], border)
-    bump1.draw()
     buff = ball.size + border
-    
-    if abs(ball.speed[1]) >= 1.5 or ball.pos[1] <= resolution[1] - buff: 
+
+    if abs(ball.speed[1]) >= 1.5 or ball.pos[1] <= resolution[1] - buff:
         ball.fall(dt)
     else:
         ball.speed[1] = 0
@@ -105,25 +121,49 @@ while running:  # Game Loop
         
     if ball.pos[1] > resolution[1] - buff:
         ball.pos[1] = resolution[1] - buff
-    
-    if ball.pos[1] >= resolution[1] - buff and ball.speed[1] > 0: 
+
+
+    # Walls
+    if ball.pos[1] >= resolution[1] - buff and ball.speed[1] > 0:
+        ball.pos[1] = resolution[1] - buff
         ball.speed[1] = -0.8 * ball.speed[1]
-        
-    distance = [bump1.pos[0] - ball.pos[0], bump1.pos[1] - ball.pos[1]]  
-    pythag = (abs(distance[0]) ** 2 + abs(distance[1]) ** 2) ** 0.5
-    hitbox = ball.size + bump1.size
-    if pythag <= hitbox:  
-        theta = math.atan(distance[1] / distance[0]) 
-        #if distance[0] == 0:
-            #if distance[1] > 0:
-               # theta = math.pi / 2
-            #elif distance[1] < 0:
-               # theta = math.pi * 1.5
-       # else: 
-        ball.speed[0] = -ball.speed * (0.8 * math.sin(theta))
-        # ball.speed[1] = -ball.speed[1] * (0.8 * math.sin(theta))  
-        
+    elif ball.pos[1] <= buff and ball.speed[1] < 0:
+        ball.pos[1] = buff
+        ball.speed[1] = -0.8 * ball.speed[1]
+
+    if ball.pos[0] <= edges[0] and ball.speed[0] < 0:
+        ball.speed[0] = -0.8 * ball.speed[0]
+    elif ball.pos[0] >= edges[1] and ball.speed[0] > 0:
+        ball.speed[0] = -0.8 * ball.speed[0]
+
+    magnitude = (ball.speed[0] ** 2 + ball.speed[1] ** 2) ** 0.5
+    heading = math.atan(ball.speed[1] / (ball.speed[0] + .001))
+    
+    for b in range(len(bumpers)):
+        bumpers[b].draw()
+        distance = [bumpers[b].pos[0] - ball.pos[0], bumpers[b].pos[1] - ball.pos[1]]
+        pythag = (abs(distance[0]) ** 2 + abs(distance[1]) ** 2) ** 0.5
+        hitbox = ball.size + bumpers[b].size
+        cushion = hitbox * 1.02
+
+        if pythag <= hitbox:
+            score += 10
+            theta = math.atan(distance[1] / (distance[0]+.001))
+            if distance[0] <= 0:
+                ball.pos[0] = bumpers[b].pos[0] + cushion * math.cos(theta)
+                ball.pos[1] = bumpers[b].pos[1] + cushion * math.sin(theta)
+                if ball.speed[0] <= 0:
+                    ball.speed[0] = magnitude * (0.8 * math.cos(theta))
+                ball.speed[1] = magnitude * (0.8 * math.sin(theta))
+            else:
+                ball.pos[0] = bumpers[b].pos[0] - cushion * math.cos(theta)
+                ball.pos[1] = bumpers[b].pos[1] - cushion * math.sin(theta)
+                if ball.speed[0] >= 0:
+                    ball.speed[0] = -magnitude * (0.8 * math.cos(theta))
+                ball.speed[1] = - magnitude * (0.8 * math.sin(theta))
+
     ball.draw()
+    draw_text(f"Score: {score}", font_mono20, (220, 230, 230), 20, 400)
 
     if event.type == pygame.KEYUP:
         button_check = False
