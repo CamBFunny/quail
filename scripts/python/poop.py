b05864f3306e4c89eca359767408ef0e9d4c6c42
CameronBFunny <156382194+CamBFunny@users.noreply.github.com>
2026-01-21 14:19:55 -0600

    Update poop.py

--- scripts/python/poop.py
+++ scripts/python/poop.py
@@ -4,7 +4,7 @@ line = ['|', '\\', '-', '/']
 m = 0
 clear = lambda: os.system('cls')
 running = 1
-load = 'loading...'
+load = ' loading...'
 length = len(load)
 stuff = list(load)
 message = ''
@@ -12,17 +12,20 @@ one = True
 two = True
 three = True
 four = True
+wait = False
+dt = 0
+t_prime = 0
+accumulator = 0
 
 while running:
     t = time.localtime(time.time())
     local_time = time.asctime(t)
-    if local_time[18] == 0:
-        m = 0
     a = line[m%4]
     index = 1 + m%length
     b = ''.join(stuff[:(index)])
-    buffer = 0.245
     timer = time.perf_counter()
+    dt = timer - t_prime
+    t_prime = timer
     string = str(timer)
     thousandths = int(''.join(string[5:7]))
     if 0 <= thousandths < 24 and one:
@@ -50,8 +53,19 @@ while running:
         one = True
         m += 1
     if m%20 == 3 and m>4:
+        wait = True
+        one = False
+        two = False
+        three = False
+        four = False
         clear()
         message += 'poop '
         print(message)
-        m = -1
-        time.sleep(2)
+        holder = thousandths
+        m = 0
+    elif wait:
+        accumulator += dt
+        if accumulator >= 1.8:
+            one = True
+            wait = False
+            accumulator = 0
