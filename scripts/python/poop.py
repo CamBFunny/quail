df4a9e695e780ee8d0df962ba056b214632fc808
CameronBFunny <156382194+CamBFunny@users.noreply.github.com>
2026-01-21 14:46:22 -0600

    Modify string formatting in poop.py

--- scripts/python/poop.py
+++ scripts/python/poop.py
@@ -26,7 +26,8 @@ while running:
     timer = time.perf_counter()
     dt = timer - t_prime
     t_prime = timer
-    string = str(timer)
+    string = str(timer) + '00'
+    print(string)
     thousandths = int(''.join(string[5:7]))
     if 0 <= thousandths < 24 and one:
         clear()
