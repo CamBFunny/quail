dfd1f43d746dd7511ef1c3fa1187fc98e69bda91
CamBFunny <cameron.beisel@gmail.com>
2026-01-25 19:06:12 -0600

    mayhem

---
+++ scripts/ascii-art.py
@@ -0,0 +1,19 @@
+import os
+
+path = '/home/cameron/PycharmProjects/quail/ascii_art'
+files = os.listdir(path)
+counter = 0
+for l in range(len(files)):
+    if files[l - counter] == 'README.md':
+        files.remove(files[l - counter])
+        counter += 1
+    else:
+        try:
+            with open(f"{path}/{files[l-counter]}", 'r') as f:
+                content = f.read()
+                print(content)
+                print('###############')
+                print(files[l-counter])
+                print('###############')
+        except:
+            print(f"\n!!! {files[l-counter]} !!!\n")
